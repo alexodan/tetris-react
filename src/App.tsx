45b8fbfcba8b9c7472f33811305899819e5d57f7
Alexis Diaz <alexdan1993@gmail.com>
2024-05-15 20:21:00 -0300

    movement

--- src/App.tsx
+++ src/App.tsx
@@ -1,86 +1,120 @@
-import { PropsWithChildren, useState } from "react";
+import { PropsWithChildren, useCallback, useEffect, useState } from "react";
 import "./App.css";
-import { useGameInterval } from "./utils";
+import { GameStatus, Tetris } from "./Tetris";
+import { useInterval } from "react-timing-hooks";
 
 export type Piece = "I" | "J" | "L" | "O" | "S" | "T" | "Z";
 
 export type BoardProps = {
-  currentPiece: Piece;
-  filledCells: any;
   board: number[][];
+  status: GameStatus;
+  resumeGame: () => void;
 };
 
 export function Board({
-  children,
-  currentPiece,
-  filledCells,
   board,
+  status,
+  resumeGame,
 }: PropsWithChildren<BoardProps>) {
   return (
-    <div className="board">
-      {board.flatMap((row, rowIdx) =>
-        row.map((cell, colIdx) => (
-          <span
-            key={`${rowIdx} - ${colIdx}`}
-            className={`cell ${cell ? "filled" : "not-filled"} ${
-              rowIdx < 2 ? "not-visible" : ""
-            }`}
-          >
-            {rowIdx}-{colIdx}
-          </span>
-        ))
-      )}
+    <div className={`board ${status}`}>
+      {status === "game-over" ? (
+        <div className="overlay">
+          <h1>Game Over</h1>
+        </div>
+      ) : status === "paused" ? (
+        <div className="overlay">
+          <h1>Game Paused</h1>
+          <button className="btn" onClick={() => resumeGame()}>
+            Resume
+          </button>
+        </div>
+      ) : null}
+      <div className="grid">
+        {board.flatMap((row, rowIdx) =>
+          row.map((cell, colIdx) => (
+            <span
+              key={`${rowIdx} - ${colIdx}`}
+              className={`cell ${
+                cell === 2 ? "stick" : cell === 1 ? "filled" : "not-filled"
+              } ${rowIdx < 2 ? "not-visible" : ""}`}
+            />
+          ))
+        )}
+      </div>
     </div>
   );
 }
 
-function getRandomPiece(): Piece {
-  const pieces = ["I", "J", "L", "O", "S", "T", "Z"] as const;
-  return pieces[Math.floor(Math.random() * pieces.length)];
+function Score({ score }: { score: number }) {
+  return <h2 className="score">Score: {score}</h2>;
 }
 
-const firstPiece = getRandomPiece();
+const tetris = new Tetris({
+  size: "M",
+  gameSpeed: 600,
+});
 
 function App() {
-  const [currentPiece, setCurrentPiece] = useState(firstPiece);
-  const [piecePosition, setPiecePosition] = useState<number[]>([0, 4]);
-  const [filledCells, setFilledCells] = useState();
+  const [, updateState] = useState({});
+  const forceUpdate = useCallback(() => updateState({}), []);
 
-  // 15 rows x 10 cols
-  const board = [
-    [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  ];
+  const { pause, start, resume } = useInterval(() => {
+    if (tetris.status === "game-over") {
+      pause();
+    } else if (tetris.status === "paused") {
+      pause();
+    } else {
+      tetris.movePiece({ dy: 1 });
+      tetris.renderPiece();
+    }
+    forceUpdate();
+  }, tetris.gameSpeed);
 
-  const { nextPosition, isRowCleared } = useGameInterval({
-    currentPiece,
-    piecePosition,
-    time: 1000,
-  });
+  useEffect(() => {
+    start();
+  }, [start]);
 
-  const nextFilledCells = filledCells;
+  useEffect(() => {
+    const listener = (e: KeyboardEvent) => {
+      if (e.key === "ArrowLeft") {
+        tetris.movePiece({ dx: -1 });
+      } else if (e.key === "ArrowRight") {
+        tetris.movePiece({ dx: 1 });
+      } else if (e.key === "Escape") {
+        if (tetris.status === "paused") {
+          tetris.status = "playing";
+          resume();
+        } else if (tetris.status === "playing") {
+          tetris.status = "paused";
+          pause();
+        }
+      } else if (e.key === "ArrowUp") {
+        tetris.rotatePiece();
+      } else if (e.key === "ArrowDown") {
+        tetris.movePiece({ dy: 1 });
+      }
+      forceUpdate();
+    };
+    document.addEventListener("keydown", listener);
+    return () => {
+      document.removeEventListener("keydown", listener);
+    };
+  }, [forceUpdate, pause, resume]);
+
+  const resumeGame = () => {
+    console.log("resume");
+    resume();
+    tetris.status = "playing";
+  };
 
   return (
     <div className="app">
+      <Score score={tetris.score} />
       <Board
-        currentPiece={currentPiece}
-        filledCells={filledCells}
-        board={board}
+        board={tetris.board}
+        status={tetris.status}
+        resumeGame={resumeGame}
       />
     </div>
   );
---
+++ src/Tetris.tsx
@@ -0,0 +1,210 @@
+// TODO:
+// colors
+// pause
+
+export type GameStatus = "playing" | "game-over" | "paused";
+
+function getRowsAndCols(size: "S" | "M" | "L") {
+  switch (size) {
+    case "S":
+      return [16, 10] as const;
+    case "M":
+      return [16, 10] as const;
+    case "L":
+      return [16, 10] as const;
+    default:
+      return [16, 10] as const;
+  }
+}
+
+// I, O, S, Z, T, L, J
+export const TETRAMINOS = [
+  [[1, 1, 1, 1]],
+  [
+    [1, 1],
+    [1, 1],
+  ],
+  [
+    [0, 1, 1],
+    [1, 1, 0],
+  ],
+  [
+    [1, 1, 0],
+    [0, 1, 1],
+  ],
+  [
+    [0, 1, 0],
+    [1, 1, 1],
+  ],
+  [
+    [0, 0, 1],
+    [1, 1, 1],
+  ],
+  [
+    [1, 0, 0],
+    [1, 1, 1],
+  ],
+];
+
+export const COLORS = [
+  "red",
+  "blue",
+  "green",
+  "yellow",
+  "orange",
+  "purple",
+  "pink",
+];
+
+export class Tetris {
+  ROWS: number;
+  COLS: number;
+  board: number[][];
+  status: "game-over" | "paused" | "playing";
+  score: number;
+  piece: {
+    x: number;
+    y: number;
+    shape: number[][];
+    color: string;
+  } = { x: 0, y: 0, shape: [], color: "" };
+  gameSpeed: number;
+
+  constructor({
+    size,
+    gameSpeed = 500,
+  }: {
+    size: "S" | "M" | "L";
+    gameSpeed: number;
+  }) {
+    [this.ROWS, this.COLS] = size ? getRowsAndCols(size) : [18, 10];
+    this.board = new Array(this.ROWS)
+      .fill("")
+      .map(() => new Array(this.COLS).fill(0));
+    this.score = 0;
+    this.createPiece();
+    this.status = "playing";
+    this.renderPiece();
+    this.gameSpeed = gameSpeed;
+  }
+
+  createPiece() {
+    const rand = Math.floor(Math.random() * TETRAMINOS.length);
+    const shape = TETRAMINOS[rand];
+    const color = COLORS[rand];
+
+    this.piece = {
+      x: this.COLS / 2 - 1,
+      y: 0,
+      shape,
+      color,
+    };
+
+    if (!this.checkMove({})) {
+      this.status = "game-over";
+    }
+  }
+
+  renderPiece({ clear, stick } = { clear: false, stick: false }) {
+    const { shape } = this.piece;
+    for (let yAxis = 0; yAxis < shape.length; yAxis++) {
+      for (let xAxis = 0; xAxis < shape[0].length; xAxis++) {
+        const dx = this.piece.x + xAxis;
+        const dy = this.piece.y + yAxis;
+        if (shape[yAxis][xAxis]) {
+          if (clear) {
+            this.board[dy][dx] = 0;
+          } else if (stick) {
+            this.board[dy][dx] = shape[yAxis][xAxis] ? 2 : 0;
+          } else {
+            this.board[dy][dx] = shape[yAxis][xAxis];
+          }
+        }
+      }
+    }
+  }
+
+  checkMove({ dx = 0, dy = 0 }: { dx?: number; dy?: number }) {
+    const { shape } = this.piece;
+    for (let yAxis = 0; yAxis < shape.length; yAxis++) {
+      for (let xAxis = 0; xAxis < shape[0].length; xAxis++) {
+        if (shape[yAxis][xAxis]) {
+          const newX = this.piece.x + xAxis + dx;
+          const newY = this.piece.y + yAxis + dy;
+          // check edges
+          if (newX < 0 || newX >= this.COLS) return false;
+          if (newY >= this.ROWS) return false;
+          // check blocked by another piece
+          const isBlocked = this.board[newY][newX] === 2;
+          if (isBlocked) return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  stickPiece() {
+    this.renderPiece({ stick: true, clear: false });
+  }
+
+  movePiece({ dx, dy }: { dx?: number; dy?: number }) {
+    const isValid = this.checkMove({ dx: dx ?? 0, dy: dy ?? 0 });
+    if (!isValid && dx) {
+      return;
+    }
+    if (!isValid && dy) {
+      this.stickPiece();
+      this.clearLines();
+      this.createPiece();
+      return;
+    }
+
+    const newX = this.piece.x + (dx ?? 0);
+    const newY = this.piece.y + (dy ?? 0);
+
+    this.renderPiece({ clear: true, stick: false });
+
+    this.piece.x = newX;
+    this.piece.y = newY;
+
+    this.renderPiece();
+  }
+
+  rotatePiece() {
+    // matrix MxN being M number of rows & N number of cols
+    const m = this.piece.shape[0].length;
+    const n = this.piece.shape.length;
+    const rotatedPiece = Array.from({ length: m }, () => Array(n).fill(0));
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < m; j++) {
+        rotatedPiece[j][n - i - 1] = this.piece.shape[i][j];
+      }
+    }
+    this.renderPiece({ clear: true, stick: false });
+    const prevShape = this.piece.shape;
+    this.piece.shape = rotatedPiece;
+    const isValid = this.checkMove({});
+    if (!isValid) {
+      this.piece.shape = prevShape;
+    }
+    this.renderPiece();
+  }
+
+  clearLines() {
+    for (let row = 0; row < this.board.length; row++) {
+      const isFull = this.board[row].every((cell) => cell === 2);
+      if (isFull) {
+        this.board.splice(row, 1);
+        this.board.unshift(new Array(this.COLS).fill(0));
+      }
+    }
+  }
+}
+
+// Pieces (movement, rotation, collisions, stop)
+
+// Engine (tick, clear lines, game status)
+
+// User input
+
+// Rendering (React)
